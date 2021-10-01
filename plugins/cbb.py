aa2133797cff50579adbf5f484d8f4c5b53b7205
ritwik-walter <77221327+ritwik-walter@users.noreply.github.com>
2021-10-01 15:37:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>W∆L13R</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Developed By : <a href='https://t.me/the_devil_of_hells_kitchen'>W∆L13R</a>\n○ Channel : @moviezcafe38\n○ Support Group : @moviezcafe38_req_zone</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
